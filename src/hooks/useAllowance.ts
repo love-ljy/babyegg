1ca3ba52a767256e63a815be33b8c7eb4105e623
JayEpher <onejayepher@gmail.com>
2024-04-26 11:49:53 +0800

    feat: add allownce

---
+++ src/hooks/useAllowance.ts
@@ -0,0 +1,53 @@
+import { erc20Abi, maxUint256 } from 'viem'
+import { useAccount, useReadContract } from 'wagmi'
+import useSubmitTransaction from './useSubmitTransaction'
+import { NULL_ADDRESS } from '@config/contants'
+
+const useAllowance = (tokenAddress: `0x${string}`, contract: `0x${string}`) => {
+  const { address, chainId } = useAccount()
+
+  const enabled = Boolean(tokenAddress && tokenAddress !== NULL_ADDRESS)
+
+  const {
+    data: allowance,
+    isLoading: isAllowanceLoading,
+    error: allowanceError,
+    refetch,
+  } = useReadContract({
+    abi: erc20Abi,
+    address: tokenAddress,
+    functionName: 'allowance',
+    args: [address as `0x${string}`, contract],
+    query: {
+      enabled,
+    },
+  })
+
+  const { isLoading: isAllowing, onSubmitTransaction: allowSpendingTokens } = useSubmitTransaction(
+    {
+      abi: erc20Abi,
+      address: tokenAddress,
+      functionName: 'approve',
+      args: [contract, maxUint256],
+      query: {
+        enabled,
+      },
+    },
+    {
+      setContext: false,
+      onError: (error: any) => {},
+      onSuccess: () => refetch(),
+    }
+  )
+
+  return {
+    isAllowanceLoading,
+    isAllowing,
+    allowance,
+    allowanceError,
+    refetch,
+    allowSpendingTokens,
+  }
+}
+
+export default useAllowance
